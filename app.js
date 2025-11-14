e3004c21b9e4e151812f23af4add2ca95c792c6e
Pintu Bhamu <pintubhamu711@gmail.com>
2025-11-14 21:00:33 +0530

    update npm

--- app.js
+++ app.js
@@ -132,5 +132,10 @@ app.use((err, req, res, next) => {
 //   console.log("server is listening on port : 8080");
 // });
 
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => console.log("Server running on port", PORT));
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server running on port", PORT));
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
